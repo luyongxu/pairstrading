ebff26b4c04c92f9d205b3606172c60234856858
Coke <luyongxu@gmail.com>
2017-10-14 15:45:32 -0400

    Model iteration 20

--- Mean Reversion/RMR.010 Cross Validate Strategy.R
+++ Mean Reversion/RMR.010 Cross Validate Strategy.R
@@ -333,10 +333,10 @@ plot_many <- function(pricing_data, time_resolution, cutoff_date, train_window, 
 } 
 
 #' # 13. Set Parameters 
-time_resolution <- 1800
-train_window <- days(64) 
-test_window <- days(32) 
-test_by <- "32 days"
+time_resolution <- 900
+train_window <- days(32) 
+test_window <- days(16) 
+test_by <- "16 days"
 threshold_z <- 2 
 
 #' # 14. Cross Validation September 2017 
