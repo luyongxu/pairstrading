640d6fc4b6918331b6f9d60b1f63eabe3ee64ee0
Coke <luyongxu@gmail.com>
2017-10-18 03:54:27 -0400

    Model iteration 46

--- Mean Reversion/RMR.010 Cross Validate Strategy.R
+++ Mean Reversion/RMR.010 Cross Validate Strategy.R
@@ -145,7 +145,7 @@ generate_signals <- function(train, test, coin_y, coin_x, threshold_z) {
   df_signals <- test %>% 
     mutate(spread = log(test[[coin_y]]) - log(test[[coin_x]]) * hedge_ratio - intercept, 
            spread_z = (spread - mean(model[["residuals"]])) / sd(model[["residuals"]]), 
-           lag_spread_z = lag(spread_z, 1), 
+           lag_spread_z = lag(spread_z, 1, default = 0), 
            signal_long = ifelse(lag_spread_z <=  0.0 & lag_spread_z > -0.5, 0.25, 0), 
            signal_long = ifelse(lag_spread_z <= -0.5 & lag_spread_z > -1.0, 0.50, signal_long), 
            signal_long = ifelse(lag_spread_z <= -1.0 & lag_spread_z > -2.0, 0.75, signal_long), 
@@ -157,7 +157,9 @@ generate_signals <- function(train, test, coin_y, coin_x, threshold_z) {
            signal_short = ifelse(lag_spread_z >= 2.0 & lag_spread_z < 4.0, -1.00, signal_short), 
            signal_short = ifelse(lag_spread_z >= 4.0, 0, signal_short), 
            signal = signal_long + signal_short, 
-           signal = ifelse(is.na(signal), 0, signal)) 
+           signal = ifelse(is.na(signal), 0, signal), 
+           signal = ifelse(cummin(lag_spread_z) <= -4.0, 0, signal), 
+           signal = ifelse(cummax(lag_spread_z) >=  4.0, 0, signal))  
   return(df_signals[["signal"]])
 } 
 
@@ -348,7 +350,7 @@ plot_many <- function(pricing_data, time_resolution, cutoff_date, train_window, 
 } 
 
 #' # 13. Set Parameters 
-quote_currency <- "BTC" 
+quote_currency <- "USDT" 
 time_resolution <- 900
 train_window <- days(32) 
 test_window <- days(16) 
