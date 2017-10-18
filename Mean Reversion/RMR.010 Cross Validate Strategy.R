e69b09060239cc36602ad8e47e8e494216dc32ec
Coke <luyongxu@gmail.com>
2017-10-18 04:07:42 -0400

    Model iteration 48

--- Mean Reversion/RMR.010 Cross Validate Strategy.R
+++ Mean Reversion/RMR.010 Cross Validate Strategy.R
@@ -149,17 +149,17 @@ generate_signals <- function(train, test, coin_y, coin_x, threshold_z) {
            signal_long = ifelse(lag_spread_z <=  0.0 & lag_spread_z > -0.5, 0.25, 0), 
            signal_long = ifelse(lag_spread_z <= -0.5 & lag_spread_z > -1.0, 0.50, signal_long), 
            signal_long = ifelse(lag_spread_z <= -1.0 & lag_spread_z > -2.0, 0.75, signal_long), 
-           signal_long = ifelse(lag_spread_z <= -2.0 & lag_spread_z > -4.0, 1.00, signal_long), 
-           signal_long = ifelse(lag_spread_z <= -4.0, 0, signal_long), 
+           signal_long = ifelse(lag_spread_z <= -2.0 & lag_spread_z > -6.0, 1.00, signal_long), 
+           signal_long = ifelse(lag_spread_z <= -6.0, 0, signal_long), 
            signal_short = ifelse(lag_spread_z >= 0.0 & lag_spread_z < 0.5, -0.25, 0), 
            signal_short = ifelse(lag_spread_z >= 0.5 & lag_spread_z < 1.0, -0.50, signal_short), 
            signal_short = ifelse(lag_spread_z >= 1.0 & lag_spread_z < 2.0, -0.75, signal_short), 
-           signal_short = ifelse(lag_spread_z >= 2.0 & lag_spread_z < 4.0, -1.00, signal_short), 
-           signal_short = ifelse(lag_spread_z >= 4.0, 0, signal_short), 
+           signal_short = ifelse(lag_spread_z >= 2.0 & lag_spread_z < 6.0, -1.00, signal_short), 
+           signal_short = ifelse(lag_spread_z >= 6.0, 0, signal_short), 
            signal = signal_long + signal_short, 
            signal = ifelse(is.na(signal), 0, signal), 
-           signal = ifelse(cummin(lag_spread_z) <= -4.0, 0, signal), 
-           signal = ifelse(cummax(lag_spread_z) >=  4.0, 0, signal))  
+           signal = ifelse(cummin(lag_spread_z) <= -6.0, 0, signal), 
+           signal = ifelse(cummax(lag_spread_z) >=  6.0, 0, signal))  
   return(df_signals[["signal"]])
 } 
 
@@ -350,7 +350,7 @@ plot_many <- function(pricing_data, time_resolution, cutoff_date, train_window, 
 } 
 
 #' # 13. Set Parameters 
-quote_currency <- "BTC" 
+quote_currency <- "USDT" 
 time_resolution <- 900
 train_window <- days(32) 
 test_window <- days(16) 
