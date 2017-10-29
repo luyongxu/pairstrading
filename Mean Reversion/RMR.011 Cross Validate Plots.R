661a1519d44f1b7e5e7afd3255d31a0605d79d70
Coke <luyongxu@gmail.com>
2017-10-28 22:43:03 -0400

    Adding more tuning parameters

--- Mean Reversion/RMR.011 Cross Validate Plots.R
+++ Mean Reversion/RMR.011 Cross Validate Plots.R
@@ -20,6 +20,10 @@ source("./Mean Reversion/RMR.010 Cross Validate Functions.R")
 pricing_data <- read_csv("./Mean Reversion/Raw Data/pricing data.csv", col_types = c("iTdddddddci")) %>% 
   filter(date_time < "2017-10-09")
 
+#' # 3. Parameter List 
+#' Description  
+#' A list of parameters passed to the functions below that describe the mean reversion pairs trading strategy.  
+#' 
 #' Arguments  
 #' time_resolution: The number of seconds that each observation spans. Takes values 300, 900, 1800, 7200, 14400, 
 #'   and 86400.   
@@ -41,10 +45,16 @@ pricing_data <- read_csv("./Mean Reversion/Raw Data/pricing data.csv", col_types
 #' signal_discrete_enter: The z-score threshold for entering a position when the signal logic is discrete.  
 #' signal_discrete_exit: The z-score threshold for exiting a position when the signal logic is discrete.  
 #' signal_stop: A threshold for the spread z-score beyond which the strategy stops trading the coin pair.  
-#' signal_reenter: A string indicating whether the strategy should reenter positions after exceeding the 
-#'   signal_stop threshold once the spread z-score returns to a reasonable range.    
-#' pair_allocation: A string indicating whether the capital allocation to the coin pairs should be equal or weighted. Takes values 
-#'   "equal" and "weighted".   
+#' signal_reenter: A boolean indicating whether the strategy should reenter positions after exceeding the 
+#'     signal_stop threshold once the spread z-score returns to a reasonable range.    
+#' signal_reenter_threshold: The z-score threshold for reentering a position if signal_reenter is TRUE. 
+#' pair_allocation: A string indicating whether the capital allocation to the coin pairs should be equal or weighted. 
+#'   Takes values "equal", "weighted", and "scaled".     
+#'  pair_allocation_scaling: A double indicating the volatility scaling applied to the cointegration stat when the pair 
+#'    allocation is scaled.   
+#'   
+
+#' # 4. Set Parameters 
 params <- list(time_resolution = 300, 
                quote_currency = "USDT", 
                cointegration_test = "distance", 
@@ -61,64 +71,66 @@ params <- list(time_resolution = 300,
                signal_discrete_exit = 0.0, 
                signal_stop = 3.51, 
                signal_reenter = TRUE, 
-               pair_allocation = "weighted") 
+               signal_reenter_threshold = 1, 
+               pair_allocation = "scaled", 
+               pair_allocation_scaling = 1.1) 
 number_pairs <- 8 
 
-#' # 4. Cross Validation September 2017
+#' # 5. Cross Validation September 2017
 plot_many(pricing_data = pricing_data,
           cutoff_date = "2017-09-01",
           params = params, 
           number_pairs = number_pairs)
 
-#' # 5. Cross Validation August 2017
+#' # 6. Cross Validation August 2017
 plot_many(pricing_data = pricing_data,
           cutoff_date = "2017-08-01",
           params = params, 
           number_pairs = number_pairs)
 
-#' # 6. Cross Validation July 2017
+#' # 7. Cross Validation July 2017
 plot_many(pricing_data = pricing_data,
           cutoff_date = "2017-07-01",
           params = params, 
           number_pairs = number_pairs)
 
-#' # 7. Cross Validation June 2017
+#' # 8. Cross Validation June 2017
 plot_many(pricing_data = pricing_data,
           cutoff_date = "2017-06-01",
           params = params, 
           number_pairs = number_pairs)
 
-#' # 8. Cross Validation May 2017
+#' # 9. Cross Validation May 2017
 plot_many(pricing_data = pricing_data,
           cutoff_date = "2017-05-01",
           params = params, 
           number_pairs = number_pairs)
 
-#' # 9. Cross Validation April 2017
+#' # 10. Cross Validation April 2017
 plot_many(pricing_data = pricing_data,
           cutoff_date = "2017-04-01",
           params = params, 
           number_pairs = number_pairs)
 
-#' # 10. Cross Validation March 2017
+#' # 11. Cross Validation March 2017
 plot_many(pricing_data = pricing_data,
           cutoff_date = "2017-03-01",
           params = params, 
           number_pairs = number_pairs)
 
-#' # 11. Cross Validation February 2017
+#' # 12. Cross Validation February 2017
 plot_many(pricing_data = pricing_data,
           cutoff_date = "2017-02-01",
           params = params, 
           number_pairs = number_pairs)
 
-#' # 12. Cross Validation January 2017
+#' # 13. Cross Validation January 2017
 plot_many(pricing_data = pricing_data,
           cutoff_date = "2017-01-01",
           params = params, 
           number_pairs = number_pairs)
 
-#' # 13. Cross Validation Full 
+#' # 14. Cross Validation Full 
 results <- backtest_strategy_full(pricing_data = pricing_data, 
                                   params = params)  
 ggplot(results, aes(x = date_time)) + 
--- Mean Reversion/RMR.012 Cross Validate Tune.R
+++ Mean Reversion/RMR.012 Cross Validate Tune.R
@@ -20,46 +20,85 @@ source("./Mean Reversion/RMR.010 Cross Validate Functions.R")
 pricing_data <- read_csv("./Mean Reversion/Raw Data/pricing data.csv", col_types = c("iTdddddddci")) %>% 
   filter(date_time < "2017-10-09")
 
-#' # 3. Describe Parameters 
-#' Arguments   
-#' pricing_data: A dataframe containing pricing data from Poloneix gathered in tidy format.  
-#' time_resolution: The number of seconds that each observation spans. Takes values 300, 900, 1800, 7200, 14400, and 86400.  
-#' cutoff_date: A data representing the cutoff date between the train and test sets.  
-#' train_window: A period object from lubridate representing the length of time the train set covers.  
-#' test_window: A period object from lubridate representing the length of time the the test set covers. 
-#' quote_currency: A string indicating the quote currency of the currency pairs. Can take values USDT or BTC.  
-#' adf_threshold: The threshold for the ADF test statistic. Pairs below this threshold are selected.   
-#' rolling_window: The number of observations used in each iteration of a rolling linear regression.  
-#' stop_threshold: A threshold for the spread z-score beyond which the strategy stops trading the coin pair.  
-#' signal_logic: A string indicating which logic to use to generate signals.  
+#' # 2. Parameter List 
+#' Description  
+#' A list of parameters passed to the functions below that describe the mean reversion pairs trading strategy.  
+#' 
+#' Arguments  
+#' time_resolution: The number of seconds that each observation spans. Takes values 300, 900, 1800, 7200, 14400, 
+#'   and 86400.   
+#' quote_currency: A string indicating the quote currency of the currency pairs. Takes values "USDT" or "BTC".  
+#' cointegration_test: A string indicating whether the Engle-Granger method or distance method is used to test for 
+#'   cointegration. Takes values "eg" or "distance".  
+#' adf_threshold: The threshold for the ADF test statistic. Pairs below this threshold are selected when using 
+#'   the Engle-Granger method.  
+#' distance_threshold: The number of coin pairs to select when using the distance method.  
+#' train_window: A lubridate period object representing the length of time the train set covers.  
+#' test_window: A lubridate period object representing the length of time the the test set covers.  
 #' model_type: A string indicating whether raw prices or log prices should be used. Takes value "raw" or "log".  
-#' number_pairs: The number of pairs to generate plots for.  
+#' spread_type: A string indicating whether the regression uses a rolling or fixed window. Takes value "rolling" or 
+#'   "fixed".  
+#' rolling_window: The number of observations used in each window of a rolling linear regression.  
+#' signal_logic: A string indicating which logic to use to generate signals. Takes values "scaled" or "discrete".  
+#' signal_scaled_enter: The z-score threshold indicating the z-score that the signal is fully scaled in when the 
+#'   signal logic is scaled.  
+#' signal_discrete_enter: The z-score threshold for entering a position when the signal logic is discrete.  
+#' signal_discrete_exit: The z-score threshold for exiting a position when the signal logic is discrete.  
+#' signal_stop: A threshold for the spread z-score beyond which the strategy stops trading the coin pair.  
+#' signal_reenter: A boolean indicating whether the strategy should reenter positions after exceeding the 
+#'     signal_stop threshold once the spread z-score returns to a reasonable range.    
+#' signal_reenter_threshold: The z-score threshold for reentering a position if signal_reenter is TRUE. 
+#' pair_allocation: A string indicating whether the capital allocation to the coin pairs should be equal or weighted. 
+#'   Takes values "equal", "weighted", and "scaled".     
+#'  pair_allocation_scaling: A double indicating the volatility scaling applied to the cointegration stat when the pair 
+#'    allocation is scaled.   
+#'   
 
 #' # 4. Tune Parameters 
-set.seed(3) 
+set.seed(1) 
 results <- tibble()
 return <- list()
 for (i in 1:10000) { 
   print(str_c("Testing iteration ", i, "."))
   time_resolution <- sample(c(300, 900, 1800, 7200, 14400, 86400), 1) 
+  quote_currency <- sample(c("USDT", "BTC"), 1)  
+  cointegration_test <- sample(c("eg", "distance"), 1) 
+  adf_threshold <- sample(seq(-1, -4, -0.05), 1) 
+  distance_threshold <- sample(seq(4, 30, 2), 1) 
   train_window <- days(sample(5:60, 1)) 
   test_window <- days(sample(5:60, 1)) 
-  quote_currency <- sample(c("USDT", "BTC"), 1)  
-  adf_threshold <- sample(seq(-1, -4, -0.01), 1)
-  rolling_window <- 86400 / time_resolution * min(as.numeric(days(sample(2:60, 1))) / 86400, as.numeric(train_window) / 86400)
-  stop_threshold <- sample(seq(3, 7, 0.01), 1) 
-  signal_logic <- sample(c("scaled", "discrete"), 1) 
   model_type <- sample(c("log", "raw"), 1) 
+  spread_type <- sample(c("rolling", "fixed"), 1) 
+  rolling_window <- 86400 / time_resolution * min(as.numeric(days(sample(2:60, 1))) / 86400, as.numeric(train_window) / 86400) 
+  signal_logic <- sample(c("scaled", "discrete"), 1) 
+  signal_scaled_enter <- sample(c(2, 3, 4), 1) 
+  signal_discrete_enter <- sample(seq(1, 3, 0.05), 1) 
+  signal_discrete_exit <- sample(seq(0, 1, 0.05), 1) 
+  signal_stop <- sample(seq(3, 7, 0.05), 1) 
+  signal_reenter <- sample(c(TRUE, FALSE), 1) 
+  signal_reenter_threshold <- sample(seq(0, 2, 0.05), 1) 
+  pair_allocation <- sample(c("equal", "weighted", "scaled"), 1) 
+  pair_allocation_scaling <- sample(seq(1, 2, 0.05), 1) 
   return[[i]] <- backtest_strategy_full(pricing_data = pricing_data, 
                                         time_resolution = time_resolution, 
+                                        quote_currency = quote_currency, 
+                                        cointegration_test = cointegration_test, 
+                                        adf_threshold = adf_threshold, 
+                                        distance_threshold = distance_threshold, 
                                         train_window = train_window, 
                                         test_window = test_window, 
-                                        quote_currency = quote_currency, 
-                                        adf_threshold = adf_threshold, 
+                                        model_type = model_type, 
+                                        spread_type = spread_type, 
                                         rolling_window = rolling_window, 
-                                        stop_threshold = stop_threshold, 
                                         signal_logic = signal_logic, 
-                                        model_type = model_type)  
+                                        signal_scaled_enter = signal_scaled_enter, 
+                                        signal_discrete_enter = signal_discrete_enter, 
+                                        signal_discrete_exit = signal_discrete_exit, 
+                                        signal_stop = signal_stop, 
+                                        signal_reenter = signal_reenter, 
+                                        signal_reenter_threshold = signal_reenter_threshold, 
+                                        pair_allocation = pair_allocation, 
+                                        pair_allocation_scaling = pair_allocation_scaling)
   results <- bind_rows(results, 
                        tibble(time_resolution = time_resolution, 
                               train_window = as.character(train_window), 
