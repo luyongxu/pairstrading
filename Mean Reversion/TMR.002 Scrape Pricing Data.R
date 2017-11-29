c77555eb02c2efd96afe753df4256c6276b52f1a
Coke <luyongxu@gmail.com>
2017-11-29 17:47:03 -0500

    Change scrape pricing data to use returnTicker endpoint for most recent data point

--- Mean Reversion/TMR.002 Scrape Pricing Data.R
+++ Mean Reversion/TMR.002 Scrape Pricing Data.R
@@ -17,17 +17,17 @@
 #' This script when called through the command line using Rscript has the option of including one argument that takes the 
 #' following values: 300, 900, 1800, 7200, 14400, 86400, update and rebuild.  
 #' 
-#' When the script is called where the argument is a number (300, 900, 1800, 7200, 14400, and 86400), the argument indicates 
+#' (1) When the script is called where the argument is a number (300, 900, 1800, 7200, 14400, and 86400), the argument indicates 
 #' the time resolution to download the data for and this argument gets passed to the period parameter in the Poloenix API 
 #' returnChartData end point. The past 24 hours of data are downloaded, compared to the exitsing documents in the mongo 
 #' collection, and uses the new data to update existing documents or is upsert into the collection. This option is designed 
 #' for cron jobs where new data is added to the database at regular intervals.  
 #' 
-#' When the script is called using the update argument, new data for all time resolutions are downloaded and only data that 
+#' (2) When the script is called using the update argument, new data for all time resolutions are downloaded and only data that 
 #' is more recent than the most recent document in the collection is inserted into the collection. This option is designed 
 #' to update the database in the event of some disruption in the Poloneix API or AWS instance hosting the script.  
 #' 
-#' When the script is called using the rebuild argument, all collections and documents within those collections are dropped, 
+#' (3) When the script is called using the rebuild argument, all collections and documents within those collections are dropped, 
 #' the collections are recreated, and all historical data is re-inserted in the collections. This option is designed to 
 #' initially populate the database or in the event of some major disruption. NB: THIS OPTION DROPS ALL EXISTING DOCUMENTS 
 #' AND REPOPULATES THE COLLECTIONS SO USE THIS OPTION WITH CAUTION.  
@@ -43,12 +43,21 @@ commandArgs <- function(...) NULL
 source("./Mean Reversion/TMR.001 Load Packages.R") 
 
 #' # 3. Query Poloniex returnTicker Endpoint 
-#' Returns the ticker for all markets. 
-return_ticker <- fromJSON("https://poloniex.com/public?command=returnTicker") %>% 
-  map2_df(names(.), ~ as_tibble(.x) %>% mutate(ticker = .y)) %>% 
-  mutate_at(vars(last, lowestAsk, highestBid, percentChange, baseVolume, quoteVolume, isFrozen, high24hr, low24hr), as.numeric)
-print(return_ticker)
-
+#' Returns the current price for a given ticker.  
+return_ticker <- function(ticker) { 
+  df <- fromJSON("https://poloniex.com/public?command=returnTicker") %>% 
+    map2_df(names(.), ~ as_tibble(.x) %>% mutate(ticker = .y)) %>% 
+    mutate_at(vars(last, lowestAsk, highestBid, percentChange, baseVolume, 
+                   quoteVolume, isFrozen, high24hr, low24hr), as.numeric) %>% 
+    mutate(date_time = Sys.time(), 
+           date_unix = as.numeric(as.POSIXct(date_time)), 
+           currency_pair = ticker, 
+           close = last) %>% 
+    select(date_unix, date_time, close, currency_pair) %>%  
+    filter(currency_pair == ticker)  
+  return(df)
+}
+  
 #' # 4. Query Poloniex returnChartData Endpoint   
 #' Returns candlestick chart data. Required GET parameters are "currencyPair", "period" (candlestick period in seconds; 
 #' valid values are 300, 900, 1800, 7200, 14400, and 86400), "start", and "end". "start" and "end" are given in unix 
@@ -91,7 +100,6 @@ if (args_period %in% c("86400", "14400", "7200", "1800", "900", "300")) {
 
 #' # 7. Download Pricing Data 
 #' Download pricing data for each ticker and period length combination. 
-
 # Initialize a tibble for containing the results for all tickers and all period length combinations 
 pricing_data <- tibble()
 
@@ -102,14 +110,19 @@ for (period in periods) {
   # Initialize a tibble for containing the results of all tickers within a period length 
   pricing_data_ticker <- tibble() 
   
-  # Download data for each ticker within each time resolution and return results in a dataframe 
+  # Download data for each ticker within each time resolution and return results in a dataframe. 
+  # Data from the returnChartdata endpoint is used for historical data and data from the returnTicker 
+  # endpoint is used for the latest value. 
   for (ticker in tickers) { 
-    print(str_c("Downloading data for currency pair ", ticker, "."))
-    pricing_data_ticker <- pricing_data_ticker %>% 
-      bind_rows(return_chartdata(currency_pair = ticker, 
-                                 start_unix = start_unix, 
-                                 end_unix = "9999999999", 
-                                 period = period)) 
+    print(str_c("Downloading data for currency pair ", ticker, ".")) 
+    df_chartdata <- return_chartdata(currency_pair = ticker, 
+                                     start_unix = start_unix, 
+                                     end_unix = "9999999999", 
+                                     period = period) %>% 
+      bind_rows(return_ticker(ticker = ticker)) %>% 
+      mutate(close = na.locf(close), 
+             period = na.locf(period))
+    pricing_data_ticker <- bind_rows(pricing_data_ticker, ) %>% bind_rows(df_chartdata)
   } 
   
   # Clean data 
@@ -139,7 +152,7 @@ for (period in periods) {
     mongo_connection$insert(new_data)
   }
 
-  # When the command line argument is a tie resolution, query the observations from the past 24 hours in the 
+  # When the command line argument is a time resolution, query the observations from the past 24 hours in the 
   # database, remove them, compare them to the most recent data, and upsert the newest data into the collection.  
   if (args_period %in% c("86400", "14400", "7200", "1800", "900", "300")) { 
     pricing_data_recent <- mongo_connection$find(query = paste0('{ "date_unix" : { "$gt" : ', start_unix, ' } }'))
