c27d3b3132007a051cd47b4ea4b4fb8ac66e2d48
Coke <luyongxu@gmail.com>
2017-12-08 18:24:31 -0500

    Add logging to shell scripts

--- Shell Scripts/scrape_data_1800.sh
+++ Shell Scripts/scrape_data_1800.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sleep 5s
-Rscript "/home/rstudio/kevin_lu_basket_mr/Mean Reversion/TMR.002 Scrape Pricing Data.R" 1800
+Rscript "/home/rstudio/kevin_lu_basket_mr/Mean Reversion/TMR.002 Scrape Pricing Data.R" 1800 >> "home/rstudio/kevin_lu_basket_mr/Logs/scrape_data_1800.log" 2>&1
--- Shell Scripts/scrape_data_86400.sh
+++ Shell Scripts/scrape_data_86400.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sleep 5s
-Rscript "/home/rstudio/kevin_lu_basket_mr/Mean Reversion/TMR.002 Scrape Pricing Data.R" 86400
+Rscript "/home/rstudio/kevin_lu_basket_mr/Mean Reversion/TMR.002 Scrape Pricing Data.R" 86400 >> "home/rstudio/kevin_lu_basket_mr/Logs/scrape_data_86400.log" 2>&1
