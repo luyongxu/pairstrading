02c5e3bcf3e1dc580db4c850cde335542d5c80f4
luyongxu <luyongxu@gmail.com>
2017-12-21 07:54:44 +0000

    Stagger start time of shell scripts

--- shell/download_data_14400.sh
+++ shell/download_data_14400.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sleep 210s
+sleep 50s
 Rscript "/home/rstudio/kevin_lu_basket_mr/src/main/01-call-download-data.R" 14400 /home/rstudio/kevin_lu_basket_mr 2>&1 | 
 sudo tee -a "/home/rstudio/kevin_lu_basket_mr/logs/download_data_14400.log"
