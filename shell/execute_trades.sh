09ca4c03baf56d05c22841da70b3c1a8a249b05d
luyongxu <luyongxu@gmail.com>
2018-01-03 05:01:36 +0000

    Fix log file path for execute trades

--- shell/execute_trades.sh
+++ shell/execute_trades.sh
@@ -5,4 +5,4 @@ Rscript "/home/rstudio/kevin_lu_basket_mr/src/main/01-call-download-data.R" 300 
 sudo tee -a "/home/rstudio/kevin_lu_basket_mr/logs/download_data_300.log"
 
 Rscript "/home/rstudio/kevin_lu_basket_mr/trade-execution/execute-trades.R" /home/rstudio/kevin_lu_basket_mr 2>&1 | 
-sudo tee -a "/home/rstudio/kevin_lu_basket_mr/logs/generate_trades.log"
+sudo tee -a "/home/rstudio/kevin_lu_basket_mr/logs/execute_trades.log"
